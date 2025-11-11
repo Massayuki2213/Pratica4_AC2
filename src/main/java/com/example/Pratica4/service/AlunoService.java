d7fb647dd6696abd42f4098dfe26873a3bad846c
Leonardo Rossi <83621860+leorossiio@users.noreply.github.com>
2025-11-10 23:06:46 -0300

    Subindo projeto pratica4_ac2

---
+++ src/main/java/com/example/Pratica4/service/AlunoService.java
@@ -0,0 +1,19 @@
+package com.example.Pratica4.service;
+
+import com.example.Pratica4.domain.Aluno;
+import com.example.Pratica4.domain.StatusAluno;
+
+public class AlunoService {
+    public void registrarParticipacaoNoForum(Aluno aluno, String mensagem) {
+        StatusAluno statusAtual = aluno.getStatus();
+        StatusAluno proximoStatus = statusAtual.adicionarCurso();
+        aluno.setStatus(proximoStatus);
+    }
+
+    public void atualizarPlanoSeElegivel(Aluno aluno) {
+        StatusAluno statusAtual = aluno.getStatus();
+        if (statusAtual.getCursos() >= 12) {
+            aluno.setPremium(true);
+        }
+    }
+}
---
+++ src/test/java/com/example/Pratica4/Pratica4ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.Pratica4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratica4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	@Test
+    void testMain() {
+        
+        Pratica4Application.main(new String[] {});
+    }
+}
